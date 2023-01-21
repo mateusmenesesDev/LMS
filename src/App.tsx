bacfa009680db43111c67da3a339a6d5fbd42df8
Mateus <mateusppa10@gmail.com>
2023-01-20 21:35:20 -0300

    :tada: Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>INÍCIO</div>;
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ['business', 'emerald'],
+  },
+  plugins: [require('daisyui')],
+};
