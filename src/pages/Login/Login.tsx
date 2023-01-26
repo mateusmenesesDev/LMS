8e728761d242c513a24334ff236121c96eab8c82
Mateus <mateusppa10@gmail.com>
2023-01-25 21:08:18 -0300

    :constructor: add function handleSubmit

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
 
 export default function Login() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <div className=''>
       {/* <div className="bg-[url('./login-bg.png')] h-screen bg-no-repeat hidden"></div> */}
-      <div className="mt-50 flex flex-col gap-5 items-center">
-        <h1 className="text-3xl text-primary-content">Conecte-se</h1>
+      <div className='mt-50 flex flex-col gap-5 items-center'>
+        <h1 className='text-3xl text-primary-content'>Conecte-se</h1>
         <p>Faça login para acessar seu espaço</p>
-        <form action="POST" className="flex flex-col gap-5 w-screen items-center">
-          <label className="label">
-            <span className="label-text">Email</span>
+        <form
+          className='flex flex-col gap-5 w-screen items-center'
+          onSubmit={handleSubmit}
+        >
+          <label className='label'>
+            <span className='label-text'>Email</span>
           </label>
-          <input type="email" className="input input-bordered w-full max-w-xs" />
-          <label className="label">
-            <span className="label-text">Password</span>
+          <input
+            type='email'
+            className='input input-bordered w-full max-w-xs'
+          />
+          <label className='label'>
+            <span className='label-text'>Password</span>
           </label>
-          <input type="password" className="input input-bordered w-full max-w-xs" />
-          <button type="submit" className="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg">Entrar</button>
+          <input
+            type='password'
+            className='input input-bordered w-full max-w-xs'
+          />
+          <button
+            type='submit'
+            className='btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg'
+          >
+            Entrar
+          </button>
         </form>
-        <p>Você não tem uma conta? <a>Problema seu!</a></p>
+        <p>
+          Você não tem uma conta? <a>Problema seu!</a>
+        </p>
       </div>
     </div>
   );
