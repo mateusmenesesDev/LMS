b706bbaec6247709411db49964c881cc5048908c
Mateus <mateusppa10@gmail.com>
2023-01-30 14:22:58 -0300

    Merging login branch with Mateus branch

--- src/routes/RouteList.tsx
+++ src/routes/RouteList.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Course from '../pages/Course/Course';
-import Courses from '../pages/Course/courses/index';
+import Courses from '../pages/Courses';
 import Login from '../pages/Login/Login';
 
 export default function RouteList() {
